987a4b9dba653c18103a946e407529ebfb96b17d
Shohagh45 <113170091+Shohagh45@users.noreply.github.com>
2023-06-16 20:27:01 +0200

    updated sql commands

---
+++ Model/Payment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+        public int OrderId { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public decimal Tip { get; set; }
+        public decimal Amount { get; set; }
+        public string Comment { get; set; }
+
+    }
+}
---
+++ Service/PaymentService.cs
@@ -0,0 +1,61 @@
+﻿using DAL;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class PaymentService
+    {
+        private readonly PaymentDao _paymentDao;
+
+        public PaymentService()
+        {
+            _paymentDao = new PaymentDao();
+        }
+
+        public List<Payment> GetAllPayments()
+        {
+            return _paymentDao.GetAllPayments();
+        }
+
+        public void AddPayment(Payment payment)
+        {
+            _paymentDao.AddPayment(payment);
+        }
+
+        public void UpdatePayment(Payment payment)
+        {
+            _paymentDao.UpdatePayment(payment);
+        }
+
+        public void DeletePayment(int paymentId)
+        {
+            _paymentDao.DeletePayment(paymentId);
+        }
+
+        public Payment GetPaymentById(int paymentId)
+        {
+            return _paymentDao.GetPaymentById(paymentId);
+        }
+
+        public List<Payment> GetPaymentsByOrderId(int orderId)
+        {
+            return _paymentDao.GetPaymentsByOrderId(orderId);
+        }
+
+        public List<Payment> GetPaymentsByEmployeeId(int employeeId)
+        {
+            return _paymentDao.GetPaymentsByEmployeeId(employeeId);
+        }
+
+        public List<string> GetTheNameOfOrderItems(int orderId)
+        {
+            return _paymentDao.GetTheNameOfOrderItems(orderId);
+        }
+    }
+}
+

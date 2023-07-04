bc6374548c49d9e129ccf27b5d5d911b582683fe
Shohagh45 <113170091+Shohagh45@users.noreply.github.com>
2023-07-04 07:21:58 +0200

    split bill model added

---
+++ Model/BillSplit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class BillSplit
+    {
+        public int BillSplitId { get; set; }
+        public Payment Payment { get; set; }
+        public int NumberOfPeople { get; set; }
+        public decimal AmountPerPerson { get; set; }
+    }
+}

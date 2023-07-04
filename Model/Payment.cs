ff1c2bbf32b4cf79281631b746066e97db33c926
Shohagh45 <113170091+Shohagh45@users.noreply.github.com>
2023-07-04 07:09:44 +0200

    payment model and service updated

--- Model/Payment.cs
+++ Model/Payment.cs
@@ -8,15 +8,12 @@ namespace Model
 {
     public class Payment
     {
-        public int OrderId;
-
         public int PaymentId { get; set; }
-        public Table Table { get; set; }
-        public Order Order { get; set; }
-        public Employee Employee { get; set; }
+        public int OrderId { get; set; }
         public PaymentMethod PaymentMethod { get; set; }
         public decimal Tip { get; set; }
         public decimal Amount { get; set; }
+        public decimal CashAmount { get; set; }
         public string Comment { get; set; }
         public DateTime PaymentDate { get; set; }
 
